0ab663535e8c1b647d03af90e59e35b4fb6a3952
AbrahamPadrino <abrahampadrino@gmail.com>
2022-01-18 11:10:20 -0500

    nueva linea agregada

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     println("Hola, nuevamente estoy practicando!")
+    println("Ahora se ha agregado un nuevo comentario")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
